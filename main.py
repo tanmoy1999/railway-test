bcb25f03e018b4d84f530e79950a62daf407930d
Tanmoy Munshi <tanmoymunshi1999@gmail.com>
2022-09-06 11:51:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 import os
+from lxml import etree
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -8,6 +10,21 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/mmi')
+def mmi():
+    URL = 'https://www.tickertape.in/market-mood-index'
+    HEADERS = ({'User-Agent':
+                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
+                'Accept-Language': 'en-US, en;q=0.5'})
+  
+    webpage = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    dom = etree.HTML(str(soup))
+    mmi = dom.xpath('//*[@id="app-container"]/div/div[1]/div[1]/div/div[2]/span')[0].text
+    day = dom.xpath('//*[@id="app-container"]/div/div[1]/div[1]/div/div[2]/p/text()[2]')[0]
+#     return render_template("MMI.html",mmi_page = mmi,last_updated = day)
+    return jsonify({mmi: day})
 
 # if __name__ == '__main__':
 #     app.run(debug=True, port=os.getenv("PORT", default=5000))
